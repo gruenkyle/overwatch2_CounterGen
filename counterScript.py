139f3e53d8a15a9c9005c0fe46c9e10ec443924d
gruenkyle <gruenkyle@vt.edu>
2023-07-15 19:25:45 -0400

    V1.52 Comments Added

--- counterScript.py
+++ counterScript.py
@@ -1,10 +1,31 @@
+'''
+07-15-2023 -- Kyle Gruen
+
+This is the original algorithm made for the counter script project
+I wanted to make sure I could make it functional before going deeper
+into the open cv side of the project.
+
+Takes in the enemy team characters and pulls the best counters for each number
+Uses large array created to rank the most repeated characters
+
+'''
+
+#Imports#
 import csv; 
 import numpy as np; 
 import collections; 
 
-largeList = []; 
-enemy_team = [1,32,36,16,23];
+#Fields#
+largeList = [];     #List of all counters pulled from csv
+enemy_team = [1,32,36,16,23];   #Current Heros on enemy team
 
+#Methods# 
+
+'''
+Method lineReader(int): 
+Takes in # specific to hero and adds all counters from that row
+in our csv file to largeList array
+'''
 def lineReader(heroNum): 
 
     f = open("./roleCounters/counterlist.csv", "r");
@@ -18,16 +39,26 @@ def lineReader(heroNum):
         elif (count > heroNum):
             break; 
         count+=1;
-        
+
+'''
+Method all_counters(arr):
+Takes array of enemy team and calls lineReader() method on each 
+number present in array
+'''       
 def all_counters(enemyNum):
 
     for num in enemy_team: 
         lineReader(num);
 
+'''
+Method rank():
+Puts all the pieces together creating the list of counters
+then uses collections to rank the top counters 
+'''
 def rank():
-    all_counters(enemy_team);
-    counters = collections.Counter(largeList); 
+    all_counters(enemy_team);   #Adds counter to largeList field
+    counters = collections.Counter(largeList);  #Creates collections counter instance 
 
-    print(counters.most_common(5)); 
+    print(counters.most_common(5)); #Number is how many heros show
 
 rank(); 
